701f061db30cb163db73effc6dd584596543fee0
paul basinger <paulcb213@gmail.com>
2019-11-11 22:26:06 -0500

    weather app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p></p>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+
 export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+
 }
 </script>
 
